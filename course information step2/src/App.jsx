26e727533d6c14d767578e0c5f0a09b82f6a9c8c
Felix Blume <137452028+fullstackopen23@users.noreply.github.com>
2023-08-24 20:33:18 +0200

    final commit

--- course information step2/src/App.jsx
+++ course information step2/src/App.jsx
@@ -1,66 +1,56 @@
-const Header = (props) => {
-  return (
-    <div>
-      <p>{props.course.name}</p>
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <div>
-      {props.part.name} {props.part.exercises}
-    </div>
-  )
-}
-
-const Content = (props) => {
-    return (
-    <div>
-      <Part part={props.course.parts[0]}/>
-      <Part part={props.course.parts[1]}/>
-      <Part part={props.course.parts[2]}/>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  const total = props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises
-  return (
-    <div>
-      <p>Number of exercises {total} </p>
-    </div>
-  )
-}
-
-
+import Course from "./components/Course"
 
 const App = () => {
-  const course = {
-    
-   name: 'Half Stack application development',
-   parts: [
+  const courses = [
     {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
     {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
     }
-   ]
-  }
+  ]
 
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <h1>Web development curriculum</h1>
+      <Course courses={courses} />
     </div>
   )
 }
